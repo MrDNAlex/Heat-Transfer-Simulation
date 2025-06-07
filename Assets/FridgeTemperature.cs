c3b9745ff07c3e206fe29383c9965bc35517fe81
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2025-06-07 00:29:08 -0400

    Interesting results

--- Assets/FridgeTemperature.cs
+++ Assets/FridgeTemperature.cs
@@ -48,36 +48,13 @@ public class FridgeTemperature : MonoBehaviour
 
     void Update()
     {
-        UpdateSinusoidalTemp();
-
-       /* if (count > width * height)
-            count = 0;
-
-        int indexX = count % width;
-        int indexY = count / height;*/
-
-        /*if (indexX % 2 == 0)
-            return;
-
-        if (indexY % 2 == 1)
-            return;
-
-        float fullMult = Time.deltaTime * multiplier;
-
-        Debug.Log($"Delta : {Time.deltaTime}, Value = {fullMult}");
-
-        tempData[indexX, indexY] = Mathf.Clamp01(tempData[indexX, indexY] + fullMult);
-
-        texture.SetPixel(indexX, indexY, new Color(0, 0, tempData[indexX, indexY]));*/
-
-        //UpdatePixels();
-       // SetConditions();
-
-        //UpdateTemperature(indexX, indexY);
-
-        
+        //UpdateSinusoidalTemp();
+        UpdateRegularTemp();
     }
 
+    /// <summary>
+    /// First method for Applying Values to Texture 2D
+    /// </summary>
     private void UpdatePixels()
     {
         for (int x = 0; x < width; x++)
@@ -93,38 +70,116 @@ public class FridgeTemperature : MonoBehaviour
         }
     }
 
-    private void SetConditions()
+    /// <summary>
+    /// Straight Line Condition
+    /// </summary>
+    private void SetLineCondition()
     {
         for (int x = 0; x < width; x++)
-            tempData[x, 0] = 1;
+            tempData[x, height / 2] = 1;
+    }
+
+    /// <summary>
+    /// Small Sqquare Condition
+    /// </summary>
+    private void SetSquareCondition()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                tempData[35 + i, 70 + j] = 1;
+            }
+        }
+    }
+
+    private void SetFloatingSquareCondition()
+    {
+        int sinX = (int)(25 * Mathf.Sin(2 * 3.14f * ((float)count) / period));
+        int sinY = (int)(25 * Mathf.Sin(2 * 3.14f * 3 * ((float)count) / period));
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                tempData[50 + i + sinX, 50 + j + sinY] = 1;
+            }
+        }
     }
 
     private void UpdateTemperature(int width, int height)
     {
-        if (width == 0) return;
+        float val = 0;
 
-        if (height == 0) return;
+        //Corners need to be implemented
+        if (width == 0 && height == 0)
+            return;
 
-        if (width == this.width - 1) return;
+        if (width == 0 && height == this.height - 1)
+            return;
 
-        if (height == this.height - 1) return;
+        if (width == this.width - 1 && height == 0)
+            return;
 
-        float val = multiplier * (tempData[width - 1, height] + tempData[width + 1, height] + tempData[width, height - 1] + tempData[width, height + 1]);
+        if (width == this.width - 1 && height == this.height - 1)
+            return;
+
+        if (width == 0)
+            val = 2 * tempData[width + 1, height] + tempData[width, height - 1] + tempData[width, height + 1];
+
+        else if (width == this.width - 1)
+            val = 2 * tempData[width - 1, height] + tempData[width, height - 1] + tempData[width, height + 1];
+
+        else if (height == 0)
+            val = tempData[width - 1, height] + tempData[width + 1, height] + 2 * tempData[width, height + 1];
+
+        else if (height == this.height - 1)
+            val = tempData[width - 1, height] + tempData[width + 1, height] + 2 * tempData[width, height - 1];
+
+        else
+            val = (tempData[width - 1, height] + tempData[width + 1, height] + tempData[width, height - 1] + tempData[width, height + 1]);
+
+        val *= multiplier;
 
         tempData[width, height] = Mathf.Clamp01(val / 4);
 
         texture.SetPixel(width, height, new Color(0, 0, tempData[width, height]));
     }
 
-    private void UpdateSinusoidalTemp ()
+    private void UpdateRegularTemp ()
     {
-        multiplier = 1 + 0.01f * Mathf.Sin( 2 * 3.14f * ((float)count) / period);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //SetSquareCondition();
+
+                SetFloatingSquareCondition();
+
+                UpdateTemperature(x, y);
+            }
+        }
+
+        count++;
+
+        if (count > period)
+            count = 0;
+
+        //Apply the Texture Update
+        texture.Apply();
+    }
+
+    private void UpdateSinusoidalTemp()
+    {
+        multiplier = 1 + 0.01f * Mathf.Sin(2 * 3.14f * ((float)count) / period);
 
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                SetConditions();
+                //SetSquareCondition();
+
+                SetFloatingSquareCondition();
 
                 UpdateTemperature(x, y);
             }
